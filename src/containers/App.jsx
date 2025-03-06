fa2e75e23ea626f0bdaf2a0da528c721f8f7441e
Fethrauga <jeffreyshepson@gmail.com>
2025-03-06 07:56:13 -0600

    converted robofriends from classes to hooks

---
+++ src/containers/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/scroll';
+import './App.css';
+
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    const [count, setCount] = useState(0);
+
+    useEffect(()=> {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {setRobots(users)});
+        console.log(count);
+    },[count]) //only run if count changes.
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
+    }
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    });
+    return !robots.length ?
+    <h1>Loading</h1> :
+    (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <button onClick={()=>setCount(count+1)}>Click Me!</button>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <CardList robots={ filteredRobots }/>
+            </Scroll>
+        </div>
+    ); 
+}
+
+export default App;
